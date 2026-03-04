670c2f2a2db555af8dff3bed7ff379d807bb0bf5
Pär Hedström <h24parhe@du.se>
2026-03-04 09:03:11 +0100

    Uppdaterade Product
    Uppdaterade tabellen Product med alla? kolumner.
    Mest på prov.

--- server/models/product.js
+++ server/models/product.js
@@ -4,6 +4,29 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            validate: {
+                len: [2, 100]
+            }
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        imageUrl: {
+            type: DataTypes.STRING(255)
+        },
+        price: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false
         }
-    });
+    },
+        {underscored: true});
 };
