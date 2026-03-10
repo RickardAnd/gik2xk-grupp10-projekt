26a2d0c3c9565453db389bf686031c76164828c4
Pär Hedström <h24parhe@du.se>
2026-03-10 13:18:51 +0100

    cart and users
    cart och users tillagda

---
+++ server/models/users.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+    // Tabell som skapas
+    return sequelize.define("users", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        username: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+
+        firstName: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+
+        lastName: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            validate: {
+                len: [3, 50]
+            }
+        },
+
+        email: {
+            type: DataTypes.STRING(200),
+            allowNull: false,
+            validate:{
+                len: [4, 200],
+                isEmail: true
+            }
+        }
+    },
+    {underscored: true});
+};
