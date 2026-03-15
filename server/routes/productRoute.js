fa611cb21aa046b5b21a2a9f31e245b9f5b1377d
RickardAnd <h24riand@du.se>
2026-03-15 19:04:02 +0100

    Har fixat en usersRoute med get,post,put,delete. Sen lagt till i productRoute med get id, put, delete. Kopplat ihop dom med app.js. Sen skapade jag en cartRoute-fil. Det går fint att köra alla i Postman

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -16,4 +16,30 @@ router.post("/", (req, res) => {
         });
 })
 
+// Hämtar en product med id
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    productService.getById(id).then((result) => {
+        res.status(result.status).json(result.data);
+    });
+})
+
+// Radera product
+router.delete('/', (req, res) => {
+        const id = req.params.id;
+
+        productService.destroy(id).then((result) => {
+        res.status(result.status).json(result.data);
+    });
+});
+
+// Ändra/Uppdatera product med id
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const product = req.body;
+    productService.update(product, id).then((result) => {
+        res.status(result.status).json(result.data);
+    });
+});
+
 module.exports = router;
---
+++ server/routes/usersRoute.js
@@ -0,0 +1,93 @@
+const router = require("express").Router();
+const db = require('../models');
+const validate = require('validate.js');
+const productService = require('../services/productService');
+
+const constraints = {
+    email: {
+      length: {
+        minimum: 4,
+        maximum: 200,
+        tooShort: "^E-postadressen måste vara minst %{count} tecken lång",
+        tooLong: "^E-postadressen får vara högst %{count} tecken lång"
+        },
+        email: {
+            message: "^E-postadressen måste vara en giltig e-postadress"
+        }
+    },
+    firstName: {
+        presence: { allowEmpty: false, message: "måste fyllas i" }
+    },
+    lastName: {
+        presence: { allowEmpty: false, message: "måste fyllas i" },
+        length: {
+            minimum: 3,
+            message: "måste vara minst 3 tecken"
+        }
+    }
+};
+
+router.get('/:id/products', (req, res) => {
+    const id = req.params.id;
+    productService.getByAuthor(id).then((result) => {
+        res.status(result.status).json(result.data);
+    });
+});
+
+router.get('/', (req, res) => {
+    db.users.findAll().then((result) => {
+        res.send(result);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.users.findByPk(id).then((user) => {
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({ message: "Användaren hittades inte" });
+        }
+    }).catch(err => {
+        res.status(500).json({ error: err.message });
+    });
+});
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    const invalidData = validate(user, constraints);
+    if(invalidData) {
+        res.status(400).json(invalidData);
+    } else {
+        db.users.create(user).then((result) => {
+        res.send(result);
+    });
+  } 
+});
+
+router.put('/:id', (req, res) => {
+    const user = req.params.id;
+    const invalidData = validate(user, constraints);
+    const id = user.id;
+    if(invalidData || !id) {
+        res.status(400).json(invalidData || "Id saknas");
+    } else {
+      db.users.update(user, {
+        where: {id: user.id}
+    })
+    .then((result) => {
+      res.send(result);
+    });
+   }
+});
+
+router.delete('/', (req, res) => {
+    db.users.destroy({ 
+        where: {id: req.body.id}
+    }).then((result) => {
+        res.json('användaren raderad');
+    });
+});
+
+module.exports = router;
