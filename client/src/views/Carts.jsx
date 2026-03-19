b7e982dbb76d486ff26db85c1b92afdbe8c1777c
Pär Hedström <h24parhe@du.se>
2026-03-19 11:28:33 +0100

    KundvagnFrontendBackend koppling
    Nu får vi upp kundvagnen i frontend.
    Fix med layout och plus/minus på saldo kvar.

--- client/src/views/Carts.jsx
+++ client/src/views/Carts.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { getCartByUserId } from "../services/CartService";
-import { Typography, Container, List, ListItem, ListItemText, Divider } from "@mui/material";
+import { Typography, Container, List, ListItem, ListItemText, Divider, Box } from "@mui/material";
 
 function Carts() {
     const [cart, setCart] = useState(null);
@@ -15,23 +15,31 @@ function Carts() {
     return (
         <Container>
             <Typography variant="h4" sx={{ my: 3 }}>Din Kundvagn</Typography>
-            {cart && cart.cart_rows && cart.cart_rows.length > 0 ? (
+            {cart && cart.products && cart.products.length > 0 ? (
+            <>
                 <List>
-                    {cart.cart_rows.map((item) => (
+                    {cart.products.map((item) => (
                         <div key={item.id}>
                             <ListItem>
                                 <ListItemText 
-                                    primary={item.product.title} 
-                                    secondary={`${item.quantity} st á ${item.product.price} kr`} 
+                                    primary={item.title} 
+                                    secondary={`${item.quantity} st á ${item.price} kr`} 
                                 />
                                 <Typography variant="body1">
-                                    {item.quantity * item.product.price} kr
+                                    {item.itemTotal} kr
                                 </Typography>
                             </ListItem>
                             <Divider />
                         </div>
                     ))}
                 </List>
+                
+                <Box sx={{ mt: 3, textAlign: 'right'}}>
+                    <Typography variant="h5">
+                            Totalbelopp: {cart.totalSum} kr
+                    </Typography>
+                </Box>
+            </>
             ) : (
                 <Typography>Kundvagnen är tom.</Typography>
             )}
