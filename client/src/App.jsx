93b5c3d3350094b3f7de1189e188b48c1f4f8f37
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2026-03-20 13:24:32 +0100

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,40 @@
-import { Link, Outlet } from "react-router-dom"
-import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
+import { Link, Outlet } from "react-router-dom";
+import { AppBar, Box, Button, Toolbar, Typography, CssBaseline } from "@mui/material";
+// 1. Importera Theme-verktygen
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import UserSelect from './components/UserSelect';
+
+// 2. Skapa ditt eget tema (Heritage Black & Gold)
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1C1C1C', // Charcoal-svart
+    },
+    secondary: {
+      main: '#C0A080', // Antikt guld
+    },
+    background: {
+      default: '#FFF8F0', // Krämvit
+    }
+  },
+  typography: {
+    // Standard-typsnitt för all vanlig text (brödtext, knappar)
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    
+    // Specifika inställningar för din h6 (din logotyp-text "VAR-Rummet")
+    h6: {
+      fontFamily: '"Playfair Display", serif', // <-- Här lägger vi in retro-fonten!
+      fontWeight: 700,
+      fontStyle: 'italic', // Lite kursivt gör det extra elegant
+      letterSpacing: '1px',
+      color: '#C0A080', 
+    },
+    // Om du vill kan du lägga till h1, h2 osv. här på samma sätt senare
+    h1: {
+      fontFamily: '"Playfair Display", serif',
+    }
+  }
+});
 
 function App() {
 
@@ -10,12 +44,16 @@ function App() {
   };
 
   return (
-    <>
+    // 3. Slå in allt i ThemeProvider
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline ser till att body får din background.default färg */}
+      <CssBaseline /> 
+      
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Link to="/" style={linkStyle}>Webbshoppen</Link>
+              <Link to="/" style={linkStyle}>knickers.se</Link>
             </Typography>
 
             {/* Dropdown för användare */}
@@ -24,17 +62,18 @@ function App() {
             </div>
 
             {/* KNAPP Se alla produkter */}
-            <Button color="inherit">
+            <Button color="secondary" variant="outlined" sx={{ ml: 2 }}>
               <Link to="/products" style={linkStyle}>Produkter</Link>
             </Button>
 
             {/* KNAPP Skapa ny (Admin) */}
-            <Button color="inherit">
-              <Link to="/products/new" style={linkStyle}>Skapa ny</Link>
+            <Button color="secondary" variant="outlined" sx={{ ml: 2 }}>
+              <Link to="/products/new" style={linkStyle}>Skapa produkt</Link>
             </Button>
 
             {/* KNAPP för Kundvagn */}
-            <Button color="inherit">
+            {/* Här kan vi använda sekundärfärgen (Guld) för att få den att sticka ut! */}
+            <Button color="secondary" variant="outlined" sx={{ ml: 2 }}>
               <Link to="/cart" style={linkStyle}>Kundvagn</Link>
             </Button>
           </Toolbar>
@@ -45,8 +84,8 @@ function App() {
       <Box sx={{ p: 3 }}> 
         <Outlet />
       </Box>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
