2d785f11778293008e873252a250a386ac433708
RickardAnd <h24riand@du.se>
2026-03-20 09:37:20 +0100

    Tog bort text

--- client/src/views/Products.jsx
+++ client/src/views/Products.jsx
@@ -3,7 +3,6 @@ import ProductList from "../components/ProductList";
 function Products() {
     return (
         <div>
-            <h2>Våra Fotbollströjor</h2>
             {/* Här lägger vi in komponenten som faktiskt hämtar och visar listan */}
             <ProductList /> 
         </div>
